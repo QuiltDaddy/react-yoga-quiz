f1df15d6a4fa48b05337e44af13b767f3e75a18f
Pat Quilty <p.quilty@yahoo.com>
2019-12-11 17:07:35 -0500

    Updates

---
+++ precache-manifest.b1441c487783d44968b1cde5edabe1a3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5acd3ec65095d014a52e8916fb21a73",
+    "url": "/react-yoga-quiz/index.html"
+  },
+  {
+    "revision": "3aa03aeec98aeb02c4bd",
+    "url": "/react-yoga-quiz/static/css/main.416fcfe0.chunk.css"
+  },
+  {
+    "revision": "cc030b3c2986369141e9",
+    "url": "/react-yoga-quiz/static/js/2.792c1e14.chunk.js"
+  },
+  {
+    "revision": "3e6887b9ed6d8bee1d899d2295e5baa1",
+    "url": "/react-yoga-quiz/static/js/2.792c1e14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3aa03aeec98aeb02c4bd",
+    "url": "/react-yoga-quiz/static/js/main.e794d4e3.chunk.js"
+  },
+  {
+    "revision": "14b79273f916787d7183",
+    "url": "/react-yoga-quiz/static/js/runtime-main.fb422d6c.js"
+  },
+  {
+    "revision": "cba2a7e5b116a01c0a69ae43bfcd993b",
+    "url": "/react-yoga-quiz/static/media/pose0.cba2a7e5.png"
+  },
+  {
+    "revision": "da3cb65a7ba72acc346a0bb2fca75c57",
+    "url": "/react-yoga-quiz/static/media/pose1.da3cb65a.png"
+  },
+  {
+    "revision": "2c56a9bbdb1e71c1720ec0828029cd98",
+    "url": "/react-yoga-quiz/static/media/pose10.2c56a9bb.png"
+  },
+  {
+    "revision": "726e38303d0b72df7499a567fe98fdd4",
+    "url": "/react-yoga-quiz/static/media/pose2.726e3830.png"
+  },
+  {
+    "revision": "3a1c786e06d80bd38bcc48e04e3a8a0e",
+    "url": "/react-yoga-quiz/static/media/pose3.3a1c786e.png"
+  },
+  {
+    "revision": "b61ea264ae02696f39f981e2bf5b056d",
+    "url": "/react-yoga-quiz/static/media/pose4.b61ea264.png"
+  },
+  {
+    "revision": "1b7ab8817b8da5f9caf04449221625e6",
+    "url": "/react-yoga-quiz/static/media/pose5.1b7ab881.png"
+  },
+  {
+    "revision": "d7c661c8aa2929baf22fe3aedb7c6cad",
+    "url": "/react-yoga-quiz/static/media/pose6.d7c661c8.png"
+  },
+  {
+    "revision": "f0257e078b77ccb143a1caea45b2f67b",
+    "url": "/react-yoga-quiz/static/media/pose7.f0257e07.png"
+  },
+  {
+    "revision": "bd590b636e81170f7414f8a304165647",
+    "url": "/react-yoga-quiz/static/media/pose8.bd590b63.png"
+  },
+  {
+    "revision": "3d69e8632e85571b8632fd8df5fa9b94",
+    "url": "/react-yoga-quiz/static/media/pose9.3d69e863.png"
+  }
+]);
